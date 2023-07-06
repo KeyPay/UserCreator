b4cc14f8d8dc920b3ddf794a6d17a7060baff4d9
Bao Hoang <baohoangbk235@gmail.com>
2023-07-06 16:10:54 +0700

    Update KeyPay enhancements

---
+++ Interfaces/IParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCreator
+{
+    public interface IParser
+    {
+        public Task HandleData(string fieldType, string dataAsString);
+    }
+}
